856a8b9ad100c08bfdeef11aa92da341dcd8475e
ano.emangi <ano.emangi@minutoseguros.com.br>
2019-09-17 18:03:51 -0300

    experimentei ASP.Net-Core

---
+++ Asp-Net001/Controllers/InstituicaoController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Asp_Net001.Models;
+
+namespace Asp_Net001.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicaos = new List<Instituicao>()
+      {
+          new Instituicao()
+          {
+              InstituicaoID = 1,
+              Nome = "UniParaná",
+              Endereco = "Paraná"
+          },
+          new Instituicao()
+          {
+              InstituicaoID = 2,
+              Nome = "UniSanta",
+              Endereco = "Santa Catarina"
+          },
+          new Instituicao()
+          {
+              InstituicaoID = 3,
+              Nome = "UniSãoPaulo",
+              Endereco = "São Paulo"
+          },
+          new Instituicao()
+          {
+              InstituicaoID = 4,
+              Nome = "UniKinshasa",
+              Endereco = "Kinshasa"
+          },
+          new Instituicao()
+          {
+              InstituicaoID = 5,
+              Nome = "UniCarioca",
+              Endereco = "Rio de Janeiro"
+          }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicaos.OrderBy(i => i.Nome));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Instituicao instituicao)
+        {
+            instituicaos.Add(instituicao);
+            instituicao.InstituicaoID = instituicaos.Select(i => i.InstituicaoID).Max()+ 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(instituicaos.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicaos.Remove(instituicaos.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicaos.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(instituicaos.Where(i => i.InstituicaoID == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(instituicaos.Where(i => i.InstituicaoID == id).First());
+        }
+    }
+}
---
+++ Asp-Net001/Models/Instituicao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Net001.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+        internal void Add(Instituicao instituicao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
